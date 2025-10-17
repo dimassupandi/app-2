9899c0f73bbd90e41399c7e91de52a77e21e5954
dimassupandi <160721651+dimassupandi@users.noreply.github.com>
2025-10-17 21:54:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// aplikasi-web-2/index.php
+echo "<h1>✅ Aplikasi Web 2 Aktif!</h1>";
+echo "<p>Berjalan di IP Public Anda pada Port 3000.</p>";
+echo "<p>Ini adalah hasil deployment otomatis dari GitHub Actions.</p>";
+echo "<p>Versi Terakhir: " . date("Y-m-d H:i:s") . "</p>";
+?>
